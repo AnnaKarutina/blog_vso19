fa02ee38610085664d8832903c35e3cd77e1b411
Anna Karutina <anna.karutina@khk.ee>
2020-01-30 10:40:31 +0200

    Add tags support

--- templates/master_template.php
+++ templates/master_template.php
@@ -46,7 +46,9 @@
   <div class="container">
     <a class="navbar-brand" href="#"><?= PROJECT_NAME ?></a>
     <ul class="navbar-nav">
-      <li class="nav-item <?= $controller == 'posts' ? 'active' : '' ?>"><a class="nav-link" href="#">Home</a></li>
+      <li class="nav-item <?= $controller == 'posts' ? 'active' : '' ?>"><a class="nav-link" href="<?php echo BASE_URL;?>">Posts</a></li>
+      <li class="nav-item <?= $controller == 'tags' ? 'active' : '' ?>"><a class="nav-link" href="<?php echo BASE_URL;?>tags">Tags</a></li>
+      <li class="nav-item <?= $controller == 'users' ? 'active' : '' ?>"><a class="nav-link" href="<?php echo BASE_URL;?>users">Users</a></li>
       <li class="nav-item <?= $controller == 'halo' ? 'active' : '' ?>"><a class="nav-link" href="halo">Halo admin</a></li>
       <!-- dropdown list item start-->
       <li class="nav-item dropdown">
